b906bc0814aaab89a9c56451813fdd7841d1affd
AlexBMA <burghelea.alex@gmail.com>
2019-05-04 20:28:54 +0300

    new update

--- firstApp/my-app/src/App.js
+++ firstApp/my-app/src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi I'm a React App </h1>
-        <p>This is realy working</p>   
+        <p>This is realy working</p> 
+        <Person name="Alex" age="26"/> 
+        <Person name="Alexandru" age="26"/> 
+        <Person name="Burghelea Alexandru" age="26" >My Hobbies: Reading</Person>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this work now'),'I\'m a React App!!');
