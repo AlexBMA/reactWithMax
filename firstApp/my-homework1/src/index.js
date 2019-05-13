31faf5db2197c84e536ccbd05e67d6c8d0d83470
AlexBMA <burghelea.alex@gmail.com>
2019-05-13 22:33:04 +0300

    Done with react hooks

--- firstApp/my-homework1/src/index.js
+++ firstApp/my-homework1/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppHook from './AppHooks';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppHook />, document.getElementById('root'));
 registerServiceWorker();
