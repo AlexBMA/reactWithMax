094d50c423eb107cab5780045b94c41d066938e0
AlexBMA <burghelea.alex@gmail.com>
2019-05-13 22:18:21 +0300

    added css

--- firstApp/my-homework1/src/UserInput/UserInput.js
+++ firstApp/my-homework1/src/UserInput/UserInput.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './UserInput.css';
 
 const userinput = (props) =>{
     return (
-    <input type="text" onChange={props.change} value={props.name}/>
+    <input type="text" onChange={props.change} value={props.name} className="inputText"/>
 )};
 
 export default userinput;
--- firstApp/my-homework1/src/UserOutput/UserOutput.js
+++ firstApp/my-homework1/src/UserOutput/UserOutput.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './UserOutput.css';
 
 const userouput = (props) =>{
     return (
     <div>
-        <p>First line user ouput {props.username}</p>
-        <p>Secound line user ouput {props.username}</p>
+        <p className="para1">First line user ouput {props.username}</p>
+        <p className="para2">Secound line user ouput {props.username}</p>
     </div>
 )};
 
