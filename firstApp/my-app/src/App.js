8bb365ddf15e5194865a738690b9ed91cf41a43c
AlexBMA <burghelea.alex@gmail.com>
2019-05-17 21:01:11 +0300

    fix

--- firstApp/my-app/src/App.js
+++ firstApp/my-app/src/App.js
@@ -18,7 +18,7 @@ class App  extends Component {
     nameChangeHandler = (event,id)=>{
       
       const personIndex = this.state.persons.findIndex(p=>{
-          return p.id==id;
+          return p.id === id;
       });
       
       const person = {
