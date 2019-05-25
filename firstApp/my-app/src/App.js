26382f9ba2203402b7a60a21eb3050cb9c701f61
AlexBMA <burghelea.alex@gmail.com>
2019-05-25 10:46:37 +0300

    css modules exmaple

--- firstApp/my-app/src/App.js
+++ firstApp/my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person.js';
 
 class App  extends Component {
@@ -49,16 +49,10 @@ class App  extends Component {
     
 
     render () {
-      // inline style with
-    const style ={
-        backgroundColor :'green',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '8px',
-        
-    };  
+    
 
     let persons = null;
+    let btnClass = '';
 
     if(this.state.showPersons){
         persons = (
@@ -76,25 +70,23 @@ class App  extends Component {
           </div> 
         );
 
-        style.backgroundColor ='red'; 
-        
+        btnClass = classes.Red;  
       }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red'); // classes =['red']
+      assignedClasses.push(classes.red); // classes =['red']
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold'); // classes =['red','bold']
+      assignedClasses.push(classes.bold); // classes =['red','bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi I'm a React App </h1>
-        <p className={classes.join(' ')}>This is realy working</p> 
+        <p className={assignedClasses.join(' ')}>This is realy working</p> 
         
-        <button 
-          style ={style}
+        <button className={btnClass}
           // this way is slower for function calls
           onClick={this.togglePersonHandler}>Switch Name</button>
         {persons}
