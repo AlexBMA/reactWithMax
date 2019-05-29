ffe19181da8cc83deac99cf09afc685370d1cc3d
Alexandru Burghelea <burghelea.alex@gmail.com>
2019-05-29 23:53:00 +0300

    hoc exmaple

--- firstApp/my-app/src/containers/App.js
+++ firstApp/my-app/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/WithClass'
 
 class App  extends Component {
 
@@ -88,7 +89,7 @@ class App  extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
       <button 
       onClick={()=>{
         this.setState({showCockpit:false})}}>
@@ -101,7 +102,7 @@ class App  extends Component {
           clicked ={this.togglePersonHandler}
         />:null}
         {persons}
-      </div>
+      </WithClass>
       );
     }
 }
