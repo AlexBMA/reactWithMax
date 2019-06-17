7b196805848313a064ae2fd8b05522ae27caa16b
Alexandru Burghelea <burghelea.alex@gmail.com>
2019-06-17 15:40:08 +0300

    dynamic burger added

--- firstApp/burger-basics--01-project-setup/src/components/Burger/Burger.js
+++ firstApp/burger-basics--01-project-setup/src/components/Burger/Burger.js
@@ -3,13 +3,26 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient.js';
 
 import classes from './Burger.css';
 
-const burger = () =>{
+const burger = (props) =>{
+
+    let transformIngredients = Object.keys(props.ingredients)
+        .map((igKey) =>{
+            return [...Array(props.ingredients[igKey])]
+                .map((_,index)=>{
+                  return <BurgerIngredient key={igKey+index} type={igKey}/>
+                });
+        }).reduce((arr,el)=>{
+            return arr.concat(el);
+        },[]);
+
+    console.log(transformIngredients);
+    if(transformIngredients.length === 0){
+        transformIngredients = <p>Please start adding ingredients!</p>;
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
